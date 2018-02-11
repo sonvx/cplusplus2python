250ba29078eeaaaff8661f01424a5f423f87d460
Son Vu <sonvx@cs.umu.se>
2018-02-11 09:12:02 +0100

    Adding build.sh

--- example1/build.sh
+++ example1/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 mkdir build; cd build
+export CPLUS_INCLUDE_PATH=/usr/include/python2.7
 cmake -DPYTHON_LIBRARY=/Library/Frameworks/Python.framework/Versions/2.7/include/python2.7 -DPYTHON_INCLUDE=/usr/include/python2.7/ -DPYTHON_LIBRARY=/usr/local/Cellar/python/2.7.13/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib ..
+make -j4
